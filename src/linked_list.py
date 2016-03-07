c03eed7b023061add6b1404d0e38842918fea74b
Norton Pengra <npengra317@gmail.com>
2016-03-07 14:46:10 -0800

    Built four operations for LinkedList with tests

--- src/linked_list.py
+++ src/linked_list.py
@@ -3,8 +3,11 @@
 class Node(object):
 
     def __init__(self, value, pointer):
-        if pointer and not pointer.isinstance(Node):
+        if pointer is None or isinstance(pointer, Node):
+            self.pointer = pointer
+        else:
             raise TypeError("Pointer must point to a node object")
+
         self.value = value
 
 
@@ -13,22 +16,49 @@ class LinkedList(object):
     tail = None
 
     def __init__(self, initial_values):
-        pass
+        for item in initial_values:
+            self.tail = Node(item, self.tail)
 
-    def insert(self):
-        pass
+    def insert(self, val):
+        self.tail = Node(value, self.tail)
 
     def pop(self):
-        pass
+        cursor = self.tail
+        previous = None
+
+        while cursor.pointer:
+            previous = cursor
+            cursor = cursor.pointer
+
+        previous.pointer = None
+
+        return cursor
+
 
     def size(self):
-        pass
+
+        cursor = self.tail
+        count = 1
+
+        while cursor.pointer:
+            count += 1
+            cursor = cursor.pointer
+
+        return count
 
     def search(self, val):
-        pass
+        cursor = self.tail
+
+        while cursor:
+            if cursor.value == val:
+                return cursor
+            cursor = cursor.pointer
+
 
     def remove(self, node):
-        pass
+        
+
+
 
     def display(self):
         pass
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -3,8 +3,44 @@ def test_LinkedList_constructor():
     from linked_list import LinkedList, Node
     input_ = [1, 2]
 
-    ListInstance = LinkedList(input_)
+    linked_list_instance = LinkedList(input_)
 
     assert LinkedList.tail is None
-    assert ListInstance.tail.value == Node(2, Node(1)).value
-    assert ListInstance.tail.pointer == Node(2, Node(1)).pointer
+    assert isinstance(linked_list_instance.tail, Node)
+    assert linked_list_instance.tail.value == Node(2, Node(1, None)).value
+    assert linked_list_instance.tail.pointer.value == Node(2, Node(1, None)).pointer.value
+    assert linked_list_instance.tail.pointer.pointer == None
+
+
+def test_LinkedList_pop():
+    from linked_list import LinkedList, Node
+    input_ = [1,2,3]
+
+    linked_list_instance = LinkedList(input_)
+
+    assert linked_list_instance.tail.value == Node(3, Node(2, Node(1, None))).value
+
+
+def test_LinkedList_count():
+    from linked_list import LinkedList, Node
+    input_ = list(range(5))
+    input2_ = list(range(75))
+
+    linked_list_instance = LinkedList(input_)
+    linked_list_instance2 = LinkedList(input2_)
+
+
+    assert linked_list_instance.size() == len(input_)
+    assert linked_list_instance2.size() == len(input2_)
+
+
+def test_LinkedList_search():
+    from linked_list import LinkedList, Node
+
+    input_ = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split()
+
+    linked_list_instance = LinkedList(input_)
+
+    assert linked_list_instance.search("d").value == "d"
+    assert linked_list_instance.search("a").value == "a"
+    assert linked_list_instance.search("norton is amazing") is None
