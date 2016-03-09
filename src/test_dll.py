cf172c260fd3dc8a650ae33562d5dd1dcabd0287
Norton Pengra <npengra317@gmail.com>
2016-03-09 15:26:40 -0800

    Added new test for DllNode

--- src/test_dll.py
+++ src/test_dll.py
@@ -3,9 +3,16 @@
 import pytest
 
 NODE_TEST_ITEMS = [
-    object,
-    235,
+    (object,),
+    (235,),
+    ("asdf",),
+    (['asdf', 'asdf'],),
+    (list(range(0, 75)),),
 ]
 
-@pytest.mark.paramertrize('')
+
+@pytest.mark.parametrize('item', NODE_TEST_ITEMS)
 def test_dll_node(item):
+    """Test that DLLNode initializes correctly."""
+    from dll import DllNode
+    assert DllNode(item).value == item
