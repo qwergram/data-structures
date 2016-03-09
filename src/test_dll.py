e37b2f39a6fe1ab90efd8eec83527140cc356151
Norton Pengra <npengra317@gmail.com>
2016-03-09 15:41:37 -0800

    Created new test for insert statement

--- src/test_dll.py
+++ src/test_dll.py
@@ -1,5 +1,5 @@
 # -*- coding=utf-8 -*-
-"""Test LinkedList for random inputs."""
+"""Test Dll for random inputs."""
 import pytest
 
 NODE_TEST_ITEMS = [
@@ -8,6 +8,8 @@ NODE_TEST_ITEMS = [
     ("asdf",),
     (['asdf', 'asdf'],),
     (list(range(0, 75)),),
+    [],
+    {},
 ]
 
 
@@ -16,3 +18,15 @@ def test_dll_node(item):
     """Test that DLLNode initializes correctly."""
     from dll import DllNode
     assert DllNode(item).value == item
+
+
+@pytest.mark.parametrize('item', NODE_TEST_ITEMS)
+def test_linkedlist_insert_integer(item):
+    """Test Dll insert command works correctly."""
+    from dll import Dll
+    input_ = [1, 2]
+    dll_instance = Dll(input_)
+    dll_instance.insert(item)
+    assert dll_instance.tail.pointer.pointer.value == item
+
+#
