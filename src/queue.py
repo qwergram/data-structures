a96b579c2dd037585372940b5744302edc9c56a5
Norton Pengra <npengra317@gmail.com>
2016-03-10 14:03:15 -0800

    Repaired dequeue to pass tests and added enqueue

--- src/queue.py
+++ src/queue.py
@@ -14,16 +14,18 @@
     This queue follows a logic of first in, first out.
 """
 
-class Node(object):
 
+class Node(object):
+    """Classify a NodeObject that points to two different objects"""
     def __init__(self, value, next, prev):
 
         self.value = value
-        self.next = next 
+        self.next = next
         self.prev = prev
 
 
 class Queue(object):
+    """Classify a new object that resembles a LinkedList following a first in first out logic."""
 
     head = None
     tail = None
@@ -32,23 +34,41 @@ class Queue(object):
         """Initalize the queue chain."""
         if isinstance(values, list):
             for value in values:
-                self.append(value)
+                self.enqueue(value)
         else:
             raise TypeError("Please package your item into a list!")
+
+    def enqueue(self, value):
+        """Enqueue a value to the tail of the linked list"""
+        new_node = Node(value, None, None)
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            new_node.next = None
+            self.tail.next = new_node
+            self.tail = new_node
 
     def dequeue(self, value):
         """Dequeue the specified item from the node chain and rebind the Nodes agian"""
         current_node = self.head
         previous_node = None
-        while current_node is not None:
-            if current_node.value == value:
-                if previous_node is not None:
-                    previous_node.next = current_node.next
-                    previous_node.next.prev = previous_node
-                else:
-                    self.head = current_node.next
-                break
-            previous_node = current_node
-            current_node = current_node.next
+        if self.tail is not None and self.tail.value == value:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        elif self.head is not None and self.head.value == value:
+            self.head = self.head.next
+            self.head.prev = None
         else:
-            raise ValueError("Item was not found in list!")
+            while current_node is not None:
+                if current_node.value == value:
+                    if previous_node is not None:
+                        previous_node.next = current_node.next
+                        previous_node.next.prev = previous_node
+                    else:
+                        self.head = current_node.next
+                    break
+                previous_node = current_node
+                current_node = current_node.next
+            else:
+                raise ValueError("Item was not found in list!")
