3d8d9c201c28dcb906152a088154db084c168601
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-13 14:04:10 -0700

    forgot to write tests. Sorry

---
+++ src/test_paren_nadia.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Test for parenthetical_nadia.py."""
+import pytest
+
+
+PAREN_TABLE = {
+    ("", 0),
+    ("()", 0),
+    ("())", 1),
+    (")()())", -1),
+    (")()", 1),
+}
+
+
+@pytest.mark.parametrize('string_, result', PAREN_TABLE)
+def test_parenthetical(string_, result):
+    """Assert parenthetical return correct int."""
+    from parenthetical_nadia import parenthetical
+    assert parenthetical(string_) == result
