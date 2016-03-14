8d5cfd6c333ca091b3fe7fcad89b9529d7ebe37a
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-14 08:58:59 -0700

    compensates for any character

--- src/parenthetical_nadia.py
+++ src/parenthetical_nadia.py
@@ -4,13 +4,17 @@
 
 def parenthetical(string_):
     """Return a number based on string."""
-    if len(string_) % 2 != 0:
+    newstr = u""
+    for i in string_:
+        if i == '(' or i == ')':
+            newstr += i
+    if len(newstr) % 2 != 0:
         return 1
-    str_size = len(string_)
-    str = string_
+    str_size = len(newstr)
+    str_ = newstr
     while str_size != 0:
-        str = str.replace('()', '')
-        if str_size == len(str):
+        str_ = str_.replace('()', '')
+        if str_size == len(str_):
             return -1
-        str_size = len(str)
+        str_size = len(str_)
     return 0
