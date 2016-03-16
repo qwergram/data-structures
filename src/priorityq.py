a2b929f13cdf2dfdc3088b4a23d79f87e0be3df9
Norton Pengra <npengra317@gmail.com>
2016-03-16 14:29:48 -0700

    Created pop and peek method for Priority Queue

--- src/priorityq.py
+++ src/priorityq.py
@@ -10,14 +10,13 @@ class PrioityQ(object):
         if isinstance(values, list):
             try:
                 for value, priority in values:
-                    if not isinstance(prioirity, int):
+                    if not isinstance(priority, int):
                         raise TypeError("Please set the priority type to integer")
                     self.insert(value, priority)
             except ValueError:
                 raise TypeError("Please package your priority into tuples!")
         else:
             raise TypeError("Please package your item into a list!")
-
 
     def insert(self, value, priority=2):
         """Insert value into Priority Queue based on Priority"""
@@ -26,5 +25,21 @@ class PrioityQ(object):
         else:
             self.priority_queue[priority] = [value]
 
+    def pop(self):
+        try:
+            highest = sorted(self.priority_queue)[0]
+            to_pop = self.priority_queue[highest][0]
+            self.priority_queue[highest] = self.priority_queue[highest][1:]
+            return to_pop
+        except IndexError:
+            raise IndexError("Hey, the queue is empty")
+
+    def peek(self):
+        try:
+            highest = sorted(self.priority_queue)[0]
+            to_pop = self.priority_queue[highest][0]
+            return to_pop
+        except IndexError:
+            return None
 
 PrioityQ([("do things", 0)])
