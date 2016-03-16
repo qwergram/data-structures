fd279913f4959de5422d7649eee35bc509e7f92d
Norton Pengra <npengra317@gmail.com>
2016-03-15 23:42:28 -0700

    added docstrings

--- src/test_binary_heap.py
+++ src/test_binary_heap.py
@@ -5,27 +5,32 @@ import pytest
 
 @pytest.fixture(scope='function')
 def num_heap():
+    """Create a 8 long unordered heap."""
     from binary_heap import BinaryHeap
     return BinaryHeap([1, 2, 3, 4, 1, 6, 7, 8])
 
 
 @pytest.fixture(scope='function')
 def short_num_heap():
+    """Create a 3 long heap."""
     from binary_heap import BinaryHeap
     return BinaryHeap([1, 2, 3])
 
 
 @pytest.fixture(scope='function')
 def med_num_heap():
+    """Create a 5 long heap."""
     from binary_heap import BinaryHeap
     return BinaryHeap([1, 2, 3, 4, 5])
 
 
 def test_max_orient_order_3(short_num_heap):
+    """Test max organization works."""
     assert short_num_heap.heap == [3, 2, 1]
 
 
 def test_max_orient_non_order_5(num_heap):
+    """Test max organization works."""
     assert num_heap.heap == [8, 4, 7, 2, 1, 6, 3, 1]
 
 
