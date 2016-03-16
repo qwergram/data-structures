6483b047ab1102b3b57540984fe33fa2dc22bc3c
Norton Pengra <npengra317@gmail.com>
2016-03-15 20:48:51 -0700

    Worked another hard night

--- src/binary_heap.py
+++ src/binary_heap.py
@@ -5,10 +5,12 @@
 class BinaryHeap(object):
     """Define a BinaryHeap object."""
 
-    heap = []
+    def _reset(self):
+        self.heap = []
 
     def __init__(self, values=[], organization='max'):
         """Heap list initialization."""
+        self._reset()
         if organization.lower() == 'max':
             self._orient = self._max_orient
         elif organization.lower() == 'min':
@@ -16,27 +18,55 @@ class BinaryHeap(object):
         else:
             raise ValueError('oraganization argument invalid')
         if isinstance(values, list):
-            for value in values:
-                self.push(value)
+            self.heap = values
+            self._orient()
+            # for value in values:
+                # self.push(value)
         else:
             raise TypeError("Please package your item into a list!")
 
     def _max_orient(self):
         """Rebind the heap with a max to min top-down orientation."""
         # import pdb; pdb.set_trace()
-        length = len(self.heap) - 1
+        length = len(self.heap)
         while True:
             start_heap = self.heap
-            for cur in range(length):
-                cur = length - cur
-                # assert type(self.heap[cur]) == int, cur
-                # if cur == 0:
-                #     break
-                i = cur // 2
-                if self.heap[i] < self.heap[cur]:
-                    self.heap[i], self.heap[cur] = self.heap[cur], self.heap[i]
-            if start_heap == self.heap:
-                break
+            for cursor in range(length):
+                # print(cursor)
+                if cursor == 0:
+                    left = 1
+                    right = 2
+                else:
+                    left = cursor * 2
+                    right = (cursor * 2) + 1
+                try:
+                    print(cursor + 1)
+                    print(left + 1)
+                    print(right + 1)
+                    print()
+                except IndexError:
+                    pass
+
+            break
+
+        # while True:
+        #     start_heap = self.heap
+        #     for cur in range(1, length):
+        #         cur = length - cur
+        #         print(self.heap)
+        #
+        #         if cur % 2 == 0:
+        #             i = cur // 2
+        #         else:
+        #             i = (cur // 2) + 1
+        #
+        #         print(i)
+        #         print("i c i c")
+        #         print(i, cur, self.heap[i], self.heap[cur])
+        #         if self.heap[i] < self.heap[cur]:
+        #             self.heap[i], self.heap[cur] = self.heap[cur], self.heap[i]
+        #     if start_heap == self.heap:
+        #         break
 
     def _min_orient(self):
         """Rebind the heap with a min to max top-down orientation."""
@@ -49,7 +79,7 @@ class BinaryHeap(object):
                     break
                 i = cur // 2
                 if self.heap[i] > self.heap[cur]:
-                    self.heap[i], self.heap[cur] = self.heap[cur], self.heap[i] 
+                    self.heap[i], self.heap[cur] = self.heap[cur], self.heap[i]
             if start_heap == self.heap:
                 break
 
@@ -62,3 +92,8 @@ class BinaryHeap(object):
         """Remove the head value and maintain structure."""
         self.heap[0] = self.heap[-1]
         self._orient()
+
+
+if __name__ == "__main__":
+    b = BinaryHeap([1,2,3,4,5])
+    print(b.heap)
--- src/test_binary_heap.py
+++ src/test_binary_heap.py
@@ -4,17 +4,26 @@ import pytest
 
 
 @pytest.fixture(scope='function')
-def test_max_orient():
-    """Test max organization works."""
+def num_heap():
     from binary_heap import BinaryHeap
-    list_ = [1, 2, 3, 4, 1, 6, 7, 8]
-    heap_instance = BinaryHeap(list_)
-    assert heap_instance.heap == [8, 7, 6, 4, 1, 3, 2, 1]
+    return BinaryHeap([1, 2, 3, 4, 1, 6, 7, 8])
+
+@pytest.fixture(scope='function')
+def short_num_heap():
+    from binary_heap import BinaryHeap
+    return BinaryHeap([1, 2, 3])
+
+@pytest.fixture(scope='function')
+def med_num_heap():
+    from binary_heap import BinaryHeap
+    return BinaryHeap([1, 2, 3, 4, 5])
 
 
-def test_max_orient_5():
+def test_max_orient(num_heap):
     """Test max organization works."""
-    from binary_heap import BinaryHeap
-    list_ = [1, 2, 3]
-    heap_instance = BinaryHeap(list_)
-    assert heap_instance.heap == [3, 2, 1]
+    assert num_heap.heap == [8, 7, 6, 4, 1, 3, 1, 2]
+
+
+def test_max_orient_short(med_num_heap):
+    """Test max organization works."""
+    assert med_num_heap.heap == [5, 4, 1, 3, 2]
