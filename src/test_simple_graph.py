856bff6118fcc5cb076a336e9b51417e64e19b8c
Norton Pengra <npengra317@gmail.com>
2016-03-17 14:08:36 -0700

    Created tests for edge and nodes

---
+++ src/test_simple_graph.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+def test_add_node():
+    from simple_graph import SimpleGraph
+    instance = SimpleGraph()
+    instance.add_node("waffles")
+    instance.add_node("waffles2")
+    instance.add_node("waffles3")
+    assert 'waffles' in instance.graph_dict
+    assert 'waffles2' in instance.graph_dict
+    assert 'waffles3' in instance.graph_dict
+
+def test_display_nodes():
+    from simple_graph import SimpleGraph
+    instance = SimpleGraph()
+    instance.add_node("waffles")
+    instance.add_node("waffles2")
+    instance.add_node("waffles3")
+    for waffle in ['waffles', 'waffles2', 'waffles3']:
+        assert waffle in instance.nodes()
+
+
+def test_add_edge():
+    from simple_graph import SimpleGraph
+    instance = SimpleGraph()
+    instance.add_node("waffles")
+    instance.add_node("waffles2")
+    instance.add_edge('waffles', 'waffles2')
+    assert instance.graph_dict['waffles'][0] == 'waffles2'
