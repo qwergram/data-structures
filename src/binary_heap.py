15f29a0179c53f6e8e7a8459b4a25fce001d8852
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-18 14:31:50 -0700

    Functions look good.  Re did all.

--- src/binary_heap.py
+++ src/binary_heap.py
@@ -6,13 +6,33 @@ class BinaryHeap(object):
     """Define a  max BinaryHeap object."""
 
     def __init___(self):
+        """Initialize heap object."""
         self.heap = []
 
-    def restructure():
+    def restructure(self):
         """Maintain structure of heap to max."""
+        if len(self.heap) > 1:  # Solves 0 and 1 case THIS IS GOOD'
+            biggest = self.heap[-1]
+            while biggest != self.heap[0]:
+                for i, value in enumerate(self.heap):
+                    if biggest < value:
+                        biggest = value
+                    try:
+                        if value < self.heap[i * 2]:
+                            self.heap[i], self.heap[2 * i] = self.heap[i * 2], value
+                        if value < self.heap[(i * 2) + 1]:
+                            self.heap[i], self.heap[(2 * i) + 1] = self.heap[(i * 2) + 1], value
+                    except IndexError:
+                        break
 
     def push(self, value):
         """Push a value into the heap."""
+        self.heap.append(value)
+        self.restructure()
 
     def pop(self):
         """Return the initial value of the heap."""
+        removed = self.heap[0]
+        self.heap = self.heap[1:]
+        self.restructure()
+        return removed
