da9222c8f81d116277c4416cc68eccd7a4fad27e
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-03-18 13:54:45 -0700

    Blow up code and start again.

--- src/binary_heap.py
+++ src/binary_heap.py
@@ -3,54 +3,16 @@
 
 
 class BinaryHeap(object):
-    """Define a BinaryHeap object.
+    """Define a  max BinaryHeap object."""
 
-        There are multiple definitions/variations of a BinaryHeap according to
-        Wikipedia. (https://en.wikipedia.org/wiki/Binary_heap) so there were
-        multiple algorithms to choose from. The algorithm or variation that this
-        heap follows is the default definition which is what is dicussed on the
-        page linked.
+    def __init___(self):
+        self.heap = []
 
-        Python Heap Q - https://docs.python.org/2/library/heapq.html
+    def restructure():
+        """Maintain structure of heap to max."""
 
-    """
-
-    heap = []
-
-    @property
-    def length(self):
-        return len(self.heap)
-
-    def __init__(self, values=[]):
-        """Heap list initialization."""
-        if isinstance(values, list):
-            for item in values:
-                if not (isinstance(item, int) or isinstance(item, float)):
-                    raise TypeError("Items in list not valid!")
-                self.push(item)
-        else:
-            raise TypeError("Please package your item into a list!")
-
-    def sift(self, startpos, pos):
-        """Rebind the heap with a max to min top-down orientation."""
-        newitem = self.heap[pos]
-        # This is from the module heapq
-        while pos > startpos:
-            parentpos = (pos - 1) >> 1
-            parent = self.heap[parentpos]
-            if newitem > parent:
-                self.heap[pos] = parent
-                pos = parentpos
-                continue
-            break
-        self.heap[pos] = newitem
-
-    def push(self, val):
-        """Pushes a value to end of self.heap while maintaining structure."""
-        self.heap.append(val)
-        self.sift(0, self.length - 1)
+    def push(self, value):
+        """Push a value into the heap."""
 
     def pop(self):
-        """Remove the head value and maintain structure."""
-        self.heap[1] = self.heap[-1]
-        self.sift(0, self.length - 1)
+        """Return the initial value of the heap."""
